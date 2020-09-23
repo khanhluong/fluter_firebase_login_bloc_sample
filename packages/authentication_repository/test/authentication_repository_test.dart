cb36b6d415830ea13e7d52932065db0c6fba2d29
KhanhLuong <baokhanh2710cntt@gmail.com>
2020-09-23 13:40:33 +1200

    add more test

--- packages/authentication_repository/test/authentication_repository_test.dart
+++ packages/authentication_repository/test/authentication_repository_test.dart
@@ -46,26 +46,146 @@ void main() {
       expect(() => AuthenticationRepository(), isNot(throwsException));
     });
 
-    group('SignUp', () {
-      test('Throw error when email is null', () {
+    group('signUp', () {
+      test('throws AssertionError when email is null', () {
         expect(
-            () => authenticationRepository.signUp(
-                email: null, password: password),
-            throwsAssertionError);
+          () => authenticationRepository.signUp(
+            email: null,
+            password: password,
+          ),
+          throwsAssertionError,
+        );
       });
 
-      test('Throw error when password is null', () {
+      test('throws AssertionError when password is null', () {
         expect(
-            () => authenticationRepository.signUp(email: email, password: null),
-            throwsAssertionError);
+          () => authenticationRepository.signUp(
+            email: email,
+            password: null,
+          ),
+          throwsAssertionError,
+        );
       });
 
       test('calls createUserWithEmailAndPassword', () async {
         await authenticationRepository.signUp(email: email, password: password);
-
         verify(firebaseAuth.createUserWithEmailAndPassword(
-                email: email, password: password))
-            .called(1);
+          email: email,
+          password: password,
+        )).called(1);
+      });
+
+      test('succeeds when createUserWithEmailAndPassword succeeds', () async {
+        expect(
+          authenticationRepository.signUp(email: email, password: password),
+          completes,
+        );
+      });
+
+      test('throws SignUpFailure when createUserWithEmailAndPassword throws',
+          () async {
+        when(firebaseAuth.createUserWithEmailAndPassword(
+          email: anyNamed('email'),
+          password: anyNamed('password'),
+        )).thenThrow(Exception());
+        expect(
+          authenticationRepository.signUp(email: email, password: password),
+          throwsA(isA<SignUpFailure>()),
+        );
+      });
+    });
+    group('loginWithGoogle', () {
+      setUp(() {
+        final googleSignInAuthentication = MockGoogleSignInAuthentication();
+        final googleSignInAccount = MockGoogleSignInAccount();
+
+        when(
+          googleSignInAccount.authentication,
+        ).thenAnswer((_) async => googleSignInAuthentication);
+
+        when(
+          googleSignIn.signIn(),
+        ).thenAnswer((_) async => googleSignInAccount);
+      });
+      test('calls signIn authentication, and signInWithCredential', () async {
+        await authenticationRepository.logInWithGoogle();
+        verify(googleSignIn.signIn()).called(1);
+        verify(firebaseAuth.signInWithCredential(any)).called(1);
+      });
+
+      test('succeeds when signIn succeeds', () {
+        expect(authenticationRepository.logInWithGoogle(), completes);
+      });
+
+      test('throws LoginWithGoogleFailure when exception occurs', () async {
+        when(firebaseAuth.signInWithCredential(any)).thenThrow(Exception());
+        expect(authenticationRepository.logInWithGoogle(),
+            throwsA(isA<LogInWithGoogleFailure>()));
+      });
+    });
+    group('logInWithEmailAndPassword', () {
+      test('thows AssertionError when email is null', () {
+        expect(
+            () => authenticationRepository.logInWithEmailAndPassword(
+                email: null, password: password),
+            throwsAssertionError);
+      });
+      test('thows AssertionError when password is null', () {
+        expect(
+            () => authenticationRepository.logInWithEmailAndPassword(
+                email: email, password: null),
+            throwsAssertionError);
+      });
+
+      test('succeeds when signInWithEmailAndPassword succeeds', () {
+        expect(
+            authenticationRepository.logInWithEmailAndPassword(
+                email: email, password: password),
+            completes);
+      });
+
+      test(
+          'throws LogInWithEmailAndPasswordFailure when'
+          'signInWithEmailAndPassword throws', () {
+        when(firebaseAuth.signInWithEmailAndPassword(
+                email: anyNamed('email'), password: anyNamed('password')))
+            .thenThrow(Exception());
+        expect(
+            authenticationRepository.logInWithEmailAndPassword(
+                email: email, password: email),
+            throwsA(isA<LogInWithEmailAndPasswordFailure>()));
+      });
+    });
+    group('logOut', () {
+      test('calls signOut', () async {
+        when(firebaseAuth.signOut()).thenAnswer((_) async => null);
+        when(googleSignIn.signOut()).thenAnswer((_) async => null);
+        await authenticationRepository.logOut();
+        verify(firebaseAuth.signOut()).called(1);
+        verify(googleSignIn.signOut()).called(1);
+      });
+
+      test('throws LogOutFailure when signOut throws', () async {
+        when(firebaseAuth.signOut()).thenThrow(Exception());
+        expect(
+          authenticationRepository.logOut(),
+          throwsA(isA<LogOutFailure>()),
+        );
+      });
+    });
+    group('user', () {
+      test('emits User.empty when firebase user is null', () async {
+        when(firebaseAuth.onAuthStateChanged).thenAnswer(
+          (_) => Stream.value(null),
+        );
+        await expectLater(authenticationRepository.user,
+            emitsInOrder(const <User>[User.empty]));
+      });
+      test('emits User when firebase user is not null', () async {
+        when(firebaseAuth.onAuthStateChanged)
+            .thenAnswer((_) => Stream.value(MockFirebaseUser()));
+        await expectLater(
+            authenticationRepository.user, emitsInOrder(const <User>[user]));
       });
     });
   });
