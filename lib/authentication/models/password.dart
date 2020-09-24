5fb1b469bb252d51872bc16685998adf07c00188
KhanhLuong <baokhanh2710cntt@gmail.com>
2020-09-24 12:55:38 +1200

    add authentication page and test

---
+++ lib/authentication/models/password.dart
@@ -0,0 +1,18 @@
+import 'package:formz/formz.dart';
+
+enum PasswordValidationError { invalid }
+
+class Password extends FormzInput<String, PasswordValidationError> {
+  Password.pure() : super.pure('');
+  Password.dirty([String value = '']) : super.dirty(value);
+
+  static final _passwordRegExp =
+      RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
+
+  @override
+  PasswordValidationError validator(String value) {
+    return _passwordRegExp.hasMatch(value)
+        ? null
+        : PasswordValidationError.invalid;
+  }
+}
