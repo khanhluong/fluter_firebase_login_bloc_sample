2655fccb1cf97484ae1242f7ec2837d07b9a4c7d
KhanhLuong <baokhanh2710cntt@gmail.com>
2020-09-25 12:31:58 +1200

    add firebease, splash screen.

---
+++ lib/login/view/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => LoginPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
